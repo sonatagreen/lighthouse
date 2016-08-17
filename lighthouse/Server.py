f1c1c17347c28d80805826e1569afacef336a4e4
Jack <jack@lbry.io>
2016-08-17 02:31:27 -0400

    return a list

--- lighthouse/Server.py
+++ lighthouse/Server.py
@@ -111,7 +111,6 @@ class Lighthouse(jsonrpc.JSONRPC):
                 'cost': self.metadata_updater.cost_and_availability[name]['cost'],
                 'available': self.metadata_updater.cost_and_availability[name]['available'],
         }
-        log.info("dict for return %s", json.dumps(r))
         return r
 
     def _process_search(self, search, keys):
@@ -134,8 +133,7 @@ class Lighthouse(jsonrpc.JSONRPC):
 
     def jsonrpc_search(self, search, search_by=DEFAULT_SEARCH_KEYS):
         if search in self.metadata_updater.metadata:
-            log.info(search)
-            return self._get_dict_for_return(search)
+            return [self._get_dict_for_return(search)]
 
         if search not in self.fuzzy_name_cache and len(self.fuzzy_name_cache) > SEARCH_RESULTS_CACHE_SIZE:
             del self.fuzzy_ratio_cache[self.fuzzy_name_cache.pop()]
